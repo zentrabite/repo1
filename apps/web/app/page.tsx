68ef8be0f6533a0c14332c11ba68d17c98777651
Liam Potter <zentrabite@gmail.com>
2026-04-20 15:37:37 +0930

    fix(web): FAQ uses named export

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ import { RevenueEngine } from "./components/revenue-engine";
 import { Industries } from "./components/industries";
 import { Showcase } from "./components/showcase";
 import { PricingTeaser } from "./components/pricing-teaser";
-import FAQ from "./components/faq";
+import { FAQ } from "./components/faq";
 import { CTABanner } from "./components/cta-banner";
 
 export default function Home() {
